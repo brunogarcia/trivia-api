8f3e1017bff45973840fc47e7928421b48244a07
Bruno Garcia <bruno@garciaechegaray.com>
2020-05-23 15:29:27 +0200

    feat: endpoint for handle GET requests for all available categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,8 +1,6 @@
-import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
 
@@ -14,8 +12,19 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
+        self.user = "postgres"
+        self.pwd = 'postgres'
+        self.host = 'localhost'
+        self.port = '5432'
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}:{}/{}".format(
+            self.user,
+            self.pwd,
+            self.host,
+            self.port,
+            self.database_name
+        )
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,14 +33,25 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
+    def test_retrieve_categories(self):
+        """GET categories """
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['categories']), 6)
+
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test
+    for successful operation and for expected errors.
     """
 
 
